384f5308c7d1f4fcb9c1e44a1e9d0756d99cef2e
Charlie Waters <cawiii@me.com>
2016-12-20 02:27:48 -0500

    Started ht32f165x flash driver

--- src/flash/nor/drivers.c
+++ src/flash/nor/drivers.c
@@ -37,6 +37,7 @@ extern struct flash_driver em357_flash;
 extern struct flash_driver faux_flash;
 extern struct flash_driver fm3_flash;
 extern struct flash_driver fm4_flash;
+extern struct flash_driver ht32f165x_flash;
 extern struct flash_driver jtagspi_flash;
 extern struct flash_driver kinetis_flash;
 extern struct flash_driver kinetis_ke_flash;
@@ -72,60 +73,61 @@ extern struct flash_driver xmc4xxx_flash;
  * @todo Make this dynamically extendable with loadable modules.
  */
 static struct flash_driver *flash_drivers[] = {
-	&aduc702x_flash,
-	&aducm360_flash,
-	&ambiqmicro_flash,
-	&at91sam3_flash,
-	&at91sam4_flash,
-	&at91sam4l_flash,
-	&at91sam7_flash,
-	&at91samd_flash,
-	&atsamv_flash,
-	&avr_flash,
-	&cfi_flash,
-	&dsp5680xx_flash,
-	&efm32_flash,
-	&em357_flash,
-	&faux_flash,
-	&fm3_flash,
-	&fm4_flash,
-	&jtagspi_flash,
-	&kinetis_flash,
-	&kinetis_ke_flash,
-	&lpc2000_flash,
-	&lpc288x_flash,
-	&lpc2900_flash,
-	&lpcspifi_flash,
-	&mdr_flash,
-	&mrvlqspi_flash,
-	&niietcm4_flash,
-	&nrf51_flash,
-	&numicro_flash,
-	&ocl_flash,
-	&pic32mx_flash,
-	&psoc4_flash,
-	&sim3x_flash,
-	&stellaris_flash,
-	&stm32f1x_flash,
-	&stm32f2x_flash,
-	&stm32lx_flash,
-	&stm32l4x_flash,
-	&stmsmi_flash,
-	&str7x_flash,
-	&str9x_flash,
-	&str9xpec_flash,
-	&tms470_flash,
-	&virtual_flash,
-	&xmc1xxx_flash,
-	&xmc4xxx_flash,
-	NULL,
+    &aduc702x_flash,
+    &aducm360_flash,
+    &ambiqmicro_flash,
+    &at91sam3_flash,
+    &at91sam4_flash,
+    &at91sam4l_flash,
+    &at91sam7_flash,
+    &at91samd_flash,
+    &atsamv_flash,
+    &avr_flash,
+    &cfi_flash,
+    &dsp5680xx_flash,
+    &efm32_flash,
+    &em357_flash,
+    &faux_flash,
+    &fm3_flash,
+    &fm4_flash,
+    &ht32f165x_flash,
+    &jtagspi_flash,
+    &kinetis_flash,
+    &kinetis_ke_flash,
+    &lpc2000_flash,
+    &lpc288x_flash,
+    &lpc2900_flash,
+    &lpcspifi_flash,
+    &mdr_flash,
+    &mrvlqspi_flash,
+    &niietcm4_flash,
+    &nrf51_flash,
+    &numicro_flash,
+    &ocl_flash,
+    &pic32mx_flash,
+    &psoc4_flash,
+    &sim3x_flash,
+    &stellaris_flash,
+    &stm32f1x_flash,
+    &stm32f2x_flash,
+    &stm32lx_flash,
+    &stm32l4x_flash,
+    &stmsmi_flash,
+    &str7x_flash,
+    &str9x_flash,
+    &str9xpec_flash,
+    &tms470_flash,
+    &virtual_flash,
+    &xmc1xxx_flash,
+    &xmc4xxx_flash,
+    NULL,
 };
 
 struct flash_driver *flash_driver_find_by_name(const char *name)
 {
-	for (unsigned i = 0; flash_drivers[i]; i++) {
-		if (strcmp(name, flash_drivers[i]->name) == 0)
-			return flash_drivers[i];
-	}
-	return NULL;
+    for (unsigned i = 0; flash_drivers[i]; i++) {
+        if (strcmp(name, flash_drivers[i]->name) == 0)
+            return flash_drivers[i];
+    }
+    return NULL;
 }
